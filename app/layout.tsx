375228b78ec4b659a49ce110e451f68c558682ef
Mlynarchik21 <ggmlynarchik21@gmail.com>
2026-02-16 23:04:37 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Mini App",
+  description: "Start page"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
