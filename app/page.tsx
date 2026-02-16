736044a7463846461620ea94d3e4d464c792dc26
Mlynarchik21 <ggmlynarchik21@gmail.com>
2026-02-16 23:04:49 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import { redirect } from "next/navigation";
-
 export default function Page() {
-  redirect("/gate");
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 24
+      }}
+    >
+      Стартовая страница запущена 🚀
+    </div>
+  );
 }
