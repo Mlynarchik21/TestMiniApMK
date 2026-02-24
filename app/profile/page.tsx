b486be89c8d761c398b040b23e2b94ffd02fd9d1
Mlynarchik21 <ggmlynarchik21@gmail.com>
2026-02-24 22:11:51 +0300

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,1 +1,17 @@
-
+export default function ProfilePage() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 24
+      }}
+    >
+      Профиль (заглушка)
+    </div>
+  );
+}
