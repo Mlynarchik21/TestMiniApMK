d440030d2989b67b0f50bc9bc70728d2f5b1e4da
Mlynarchik21 <ggmlynarchik21@gmail.com>
2026-02-24 22:19:36 +0300

    Update page.tsx

--- app/robot/page.tsx
+++ app/robot/page.tsx
@@ -1,1 +1,14 @@
-
+export default function RobotPage() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        minHeight: "100vh",
+        padding: 16
+      }}
+    >
+      <h1 style={{ margin: 0, fontSize: 18 }}>Robot (заглушка)</h1>
+    </div>
+  );
+}
