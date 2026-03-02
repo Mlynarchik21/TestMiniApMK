45059dd2239256ec9ef4f05831d0c4fbeadac6d0
Mlynarchik21 <ggmlynarchik21@gmail.com>
2026-03-02 22:17:34 +0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,2 @@
-import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-function makeClient() {
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) throw new Error("DATABASE_URL is missing");
-
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-
-  return new PrismaClient({ adapter, log: ["error", "warn"] });
-}
-
-export const prisma = global.prisma ?? makeClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+// lib/db.ts
+export { prisma } from "@/lib/prisma";
