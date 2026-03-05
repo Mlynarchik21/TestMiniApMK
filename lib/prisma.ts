6c41cbcdb0c83f9fe96a5fcc89c5d1c568528af4
Mlynarchik21 <ggmlynarchik21@gmail.com>
2026-03-06 00:26:47 +0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,29 +3,33 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-// Важно: НЕ логируем и НЕ печатаем DATABASE_URL
-// Pool берёт connectionString из env (Supabase pooler)
+// Не логируем DATABASE_URL
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
   pgPool?: Pool;
 };
 
+// Pool использует Supabase pooler
 const pool =
   globalForPrisma.pgPool ??
   new Pool({
     connectionString: process.env.DATABASE_URL,
   });
 
-// Prisma v7 + driver adapter (pg)
+// Prisma v7 driver adapter
 const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
-// В dev не создаём новые подключения при hot-reload
+// защита от множественных подключений в dev
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
   globalForPrisma.pgPool = pool;
